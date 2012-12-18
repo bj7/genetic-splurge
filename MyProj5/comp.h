930462dd5b00e84749c97309731197ea458ac3c9
bj7 <jjbernitt@gmail.com>
2012-12-18 12:50:59 -0500

    stuff

---
+++ MyProj5/comp.h
@@ -0,0 +1,29 @@
+/*this is a header file containing data for use by the compiler*/
+#ifndef COMP_H_INCLUDE
+#define COMP_H_INCLUDE
+#include "uthash.h"
+
+typedef enum{
+    INTEGER,
+    FLOAT,
+    BOOLEAN,
+}DATATYPES;
+
+typedef enum{
+	FALSE, //0
+	TRUE,	//1
+}BOOLTYPE;
+
+typedef struct {
+    char* name;
+    int type;
+    int lineno;
+    int offset;
+    UT_hash_handle hh;
+}SymbolTableEntry;
+
+SymbolTableEntry *symbolTableGetEntry(SymbolTableEntry **symbol_table, char* key);
+
+void symbolTableAddEntry(SymbolTableEntry **symbol_table, SymbolTableEntry *entry);
+
+#endif
